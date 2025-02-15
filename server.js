85510196f0a96cfa39146f0520ad3c6e280dc061
Sanjayan <sanjayan201@gmail.com>
2025-02-15 21:19:39 +0530

    Fix1

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 console.log("Using PORT:", process.env.PORT);
 app.listen(port, '0.0.0.0', () => {
   console.log(`Minimal server listening on port ${port}`);
