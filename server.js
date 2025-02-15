c1977689a2f6e843dcbc43af11f572850a3c31bb
Sanjayan <sanjayan201@gmail.com>
2025-02-15 21:04:53 +0530

    8080

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const app = express();
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 8080;
+console.log("Using PORT:", process.env.PORT);
 app.listen(port, '0.0.0.0', () => {
   console.log(`Minimal server listening on port ${port}`);
 });
